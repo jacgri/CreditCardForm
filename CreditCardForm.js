1f5263a47b2a2f5d564a99434653f1a396c475f9
Jacqui Grimes <grimesjacqui@gmail.com>
2017-11-13 20:19:15 +0000

    adding validation autocomplete and regex patterns to form to improve user experience

--- CreditCardForm.js
+++ CreditCardForm.js
@@ -1,4 +1,8 @@
-document.getElementById('cc-form').addEventListener('submit', event => {
+document.getElementById('js-cc-form').addEventListener('submit', event => {
   event.preventDefault()
   console.log(event.target.fullName.value)
+  console.log(event.target.city.value)
+  console.log(event.target.creditCardNumber.value)
+  console.log(event.target.expiryDate.value)
+  console.log(event.target.securityCode.value)
 })
